51bdec57cd8aa936d6620134b95ed1d8a3d6e1e5
abdullahbarakat166 <abdullahuber1998@gmail.com>
2024-06-24 18:49:43 +0300

    Initial commit

---
+++ Location.kt
@@ -0,0 +1,223 @@
+package com.hrishi.pdfreader
+
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Looper
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.widget.doOnTextChanged
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
+import com.hrishi.pdfreader.databinding.ActivityLocationBinding
+
+const val REQUEST_LOCATION = 0   //Request Code Location
+const val REQUEST_CHECK_SETTINGS = 1
+var LocationUpdateInterval: Long = 10000 //milliSec
+var MIN_UPDATE_INTERVAL = 5000
+var requestingLocationUpdates = false
+
+class Location : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var binding: ActivityLocationBinding
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLocationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        requestPermissions()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult
+
+                for (location in locationResult.locations) {
+                    //Do something with location HERE
+                    binding.tvLocation.text = "${location.latitude} ${location.longitude}"
+                    binding.tvTime.text = java.util.Calendar.getInstance().time.toString()
+                }
+            }
+        }
+
+        binding.switchLocationUpdate.setOnCheckedChangeListener { _, isChecked ->
+            if(checkFineLocationPermission() && checkCoarseLocationPermission()){
+                if (isChecked) {
+                    requestingLocationUpdates = true
+                    createLocationRequest()
+                } else {
+                    requestingLocationUpdates = false
+                    stopLocationUpdates()
+                }
+            }else{
+                requestPermissions()
+                binding.switchLocationUpdate.isChecked = false
+                showPermissionExplanation()
+            }
+
+        }
+
+        binding.etIntervalTime.doOnTextChanged { text, start, before, count ->
+            if (text!!.isNotEmpty()) {
+                val interval = Integer.parseInt(text.toString())
+                if (interval >= MIN_UPDATE_INTERVAL) {
+                    LocationUpdateInterval = interval.toLong()
+
+                    if (::locationRequest.isInitialized) {
+                        locationRequest.interval = LocationUpdateInterval
+                        locationRequest.fastestInterval = LocationUpdateInterval
+                        stopLocationUpdates()
+                        createLocationRequest()
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    //Need coarse location permission for Fine Location
+    private fun checkCoarseLocationPermission() = ActivityCompat.checkSelfPermission(
+        this, android.Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun checkFineLocationPermission() = ActivityCompat.checkSelfPermission(
+        this, android.Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun checkApiVersion() =
+        android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M
+
+    private fun requestPermissions() {
+        if (checkApiVersion()) {
+
+            val permissionList = mutableListOf<String>()
+
+            if (!checkCoarseLocationPermission()) {
+                permissionList.add(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            }
+
+            if (!checkFineLocationPermission()) {
+                permissionList.add(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+
+            if (permissionList.isNotEmpty()) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    permissionList.toTypedArray(),
+                    REQUEST_LOCATION
+                )
+            }
+        }
+    }
+
+    private fun showPermissionExplanation() {
+        Toast.makeText(this, "Need that Permission Bitch!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun createLocationRequest() {
+
+        locationRequest = LocationRequest.create().apply {
+            interval = LocationUpdateInterval
+            fastestInterval = LocationUpdateInterval
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        //checks if GPS Enabled, and prompts for the same
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locationSettingsResponse ->
+            startLocationUpdates(locationRequest)
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                try {
+                    exception.startResolutionForResult(
+                        this@Location,
+                        REQUEST_CHECK_SETTINGS
+                    )
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+    }
+
+    private fun startLocationUpdates(locationRequest: LocationRequest) {
+        if (checkFineLocationPermission()) {
+            binding.tvLocation.text = "Getting Location..."
+
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        }else{
+            requestPermissions()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == Activity.RESULT_OK) {
+                createLocationRequest()
+            } else {
+                Toast.makeText(this, "GPS Error", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+
+        binding.tvLocation.text = "Location Updates Stopped"
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (requestingLocationUpdates) createLocationRequest()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_LOCATION) {
+            for (i in grantResults.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    if (checkApiVersion()) {
+                        if (shouldShowRequestPermissionRationale(permissions[i])) {
+                            showPermissionExplanation()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+
+}
---
+++ MainActivity.kt
@@ -0,0 +1,265 @@
+package com.hrishi.pdfreader
+
+import android.content.ActivityNotFoundException
+import android.net.Uri
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.isVisible
+import com.hrishi.pdfreader.databinding.ActivityMainBinding
+import com.itextpdf.text.pdf.PdfReader
+import com.itextpdf.text.pdf.parser.PdfTextExtractor
+
+const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var tts: TextToSpeech
+    lateinit var reader: PdfReader
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fabPlayTts.isEnabled = false
+        hideControls()
+
+        tts = TextToSpeech(this, this)
+
+        tts.setOnUtteranceProgressListener(
+            object : UtteranceProgressListener() {
+                override fun onStart(p0: String?) {
+                    binding.fabPlayTts.setImageDrawable(
+                        AppCompatResources.getDrawable(
+                            this@MainActivity,
+                            R.drawable.ic_baseline_stop_24
+                        )
+                    )
+                }
+
+                override fun onStop(utteranceId: String?, interrupted: Boolean) {
+                    super.onStop(utteranceId, interrupted)
+                    binding.fabPlayTts.setImageDrawable(
+                        AppCompatResources.getDrawable(
+                            this@MainActivity,
+                            R.drawable.ic_baseline_play_arrow_24
+                        )
+                    )
+                }
+
+                override fun onDone(p0: String?) {
+                    binding.fabPlayTts.setImageDrawable(
+                        AppCompatResources.getDrawable(
+                            this@MainActivity,
+                            R.drawable.ic_baseline_play_arrow_24
+                        )
+                    )
+
+                    nextPageSpeak()
+                    //speak(binding.tvPageContent.text.toString())
+                }
+
+                override fun onError(p0: String?) {
+                    Toast.makeText(
+                        this@MainActivity, "Error : $p0", Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        )
+
+        val selectPdfResult = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            if (loadPdf(it)) {
+                setPageContent(1)
+            }
+        }
+
+        binding.fabSelectFile.setOnClickListener {
+            pickFile(selectPdfResult)
+        }
+
+        binding.fabNextPage.setOnClickListener {
+            nextPage()
+        }
+
+        binding.fabPreviousPage.setOnClickListener {
+            if (::reader.isInitialized) {
+                var currentPgNo = Integer.parseInt(binding.tvCurrentPgNo.text.toString())
+                if (1 < currentPgNo) {
+                    currentPgNo--
+                    setPageContent(currentPgNo)
+                    binding.tvCurrentPgNo.text = currentPgNo.toString()
+
+                    if (tts.isSpeaking) {
+                        tts.stop()
+                        speak(binding.tvPageContent.text.toString())
+                    }
+                }
+            }
+        }
+
+        binding.fabPlayTts.setOnClickListener {
+
+            if (::reader.isInitialized) {
+                if (tts.isSpeaking) {
+                    tts.stop()
+                    binding.fabPlayTts.setImageDrawable(
+                        AppCompatResources.getDrawable(
+                            this@MainActivity,
+                            R.drawable.ic_baseline_play_arrow_24
+                        )
+                    )
+
+                } else {
+                    speak(binding.tvPageContent.text.toString())
+
+                }
+            }
+        }
+
+        binding.fabAddBig.setOnClickListener {
+            pickFile(selectPdfResult)
+        }
+    }
+
+    //Changes to Next Page
+    private fun nextPageSpeak() {
+        if (::reader.isInitialized) {
+            var currentPgNo = Integer.parseInt(binding.tvCurrentPgNo.text.toString())
+            if (currentPgNo < reader.numberOfPages) {
+                currentPgNo++
+                setPageContent(currentPgNo)
+                binding.tvCurrentPgNo.text = currentPgNo.toString()
+
+                if (tts.isSpeaking) {
+                    tts.stop()
+                }
+                speak(binding.tvPageContent.text.toString())
+            }
+        }
+    }
+
+    //Changes to Next Page
+    private fun nextPage() {
+        if (::reader.isInitialized) {
+            var currentPgNo = Integer.parseInt(binding.tvCurrentPgNo.text.toString())
+            if (currentPgNo < reader.numberOfPages) {
+                currentPgNo++
+                setPageContent(currentPgNo)
+                binding.tvCurrentPgNo.text = currentPgNo.toString()
+
+                if (tts.isSpeaking) {
+                    tts.stop()
+                    speak(binding.tvPageContent.text.toString())
+                }
+            }
+        }
+    }
+
+    //Opens File Picker
+    private fun pickFile(selectPdfResult: ActivityResultLauncher<String>) {
+        if(tts.isSpeaking){
+            tts.stop()
+        }
+
+        try {
+            selectPdfResult.launch("application/pdf")
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "No File Picker Found", Toast.LENGTH_SHORT).show()
+            Log.e(TAG, "pickFile: ${e.message}")
+        }
+    }
+
+    //Sets the reader
+    private fun loadPdf(uri: Uri): Boolean {
+
+        try {
+            reader = PdfReader(contentResolver.openInputStream(uri))
+
+            if (reader.numberOfPages == 0) {
+                Toast.makeText(this, "Empty Pdf", Toast.LENGTH_SHORT).show()
+                return false
+            }
+
+            showControls()
+
+            binding.tvTotalPages.text = reader.numberOfPages.toString()
+            binding.tvPgNoSeperator.text = "/"
+            binding.tvCurrentPgNo.text = "1"
+            return true
+
+        } catch (e: Exception) {
+            Toast.makeText(this, "Error While Reading PDF", Toast.LENGTH_SHORT).show()
+            Log.e(TAG, "loadPdf: ${e.message}")
+            return false
+        }
+
+    }
+
+    //Sets Text of Page to the TextView
+    private fun setPageContent(pageNo: Int) {
+
+        if (pageNo <= reader.numberOfPages) {
+            binding.tvPageContent.text = "Page $pageNo \n\n" + PdfTextExtractor.getTextFromPage(
+                reader,
+                pageNo
+            ).trim()
+        }
+    }
+
+    //For TextToSpeech Init
+    override fun onInit(p0: Int) {
+        binding.fabPlayTts.isEnabled = true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tts.shutdown()
+    }
+
+    //Speaks the text with TextToSpeech
+    private fun speak(text: String) =
+        tts.speak(text.trim(), TextToSpeech.QUEUE_FLUSH, null, "PdfReader")
+    
+    private fun hideControls(){
+        binding.fabPlayTts.isVisible = false
+        binding.fabPreviousPage.isVisible = false
+        binding.fabNextPage.isVisible = false
+        binding.fabSelectFile.isVisible = false
+        binding.tvCurrentPgNo.isVisible = false
+        binding.tvPgNoSeperator.isVisible = false
+        binding.tvTotalPages.isVisible = false
+
+        binding.tvHeadTop.isVisible = false
+        binding.ivHeadTop.isVisible = false
+        binding.lineHeadTop.isVisible = false
+    }
+
+    private fun showControls(){
+
+        binding.fabAddBig.isVisible = false
+        binding.tvHead1.isVisible = false
+        binding.tvHead2.isVisible = false
+        binding.tvHead3.isVisible = false
+
+        binding.fabPlayTts.isVisible = true
+        binding.fabPreviousPage.isVisible = true
+        binding.fabNextPage.isVisible = true
+        binding.fabSelectFile.isVisible = true
+        binding.tvCurrentPgNo.isVisible = true
+        binding.tvPgNoSeperator.isVisible = true
+        binding.tvTotalPages.isVisible = true
+
+        binding.tvHeadTop.isVisible = true
+        binding.ivHeadTop.isVisible = true
+        binding.lineHeadTop.isVisible = true
+    }
+}
+
